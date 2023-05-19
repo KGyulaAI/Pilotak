c8704fcadd7b97f1f22e978aec0db504fbd98b94
Gyula <Gyula>
2023-05-19 10:35:38 +0200

    Még egy push.

--- Pilotak/Pilotak.cs
+++ Pilotak/Pilotak.cs
@@ -8,5 +8,29 @@ namespace Pilotak
 {
     internal class Pilotak
     {
+        string nev;
+        string szuletesiDatum;
+        string nemzetiseg;
+        int rajtszam;
+
+        public Pilotak(string nev, string szuletesiDatum, string nemzetiseg, int rajtszam)
+        {
+            this.nev = nev;
+            this.szuletesiDatum = szuletesiDatum;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+        public Pilotak(string nev, string szuletesiDatum, string nemzetiseg)
+        {
+            this.nev = nev;
+            this.szuletesiDatum = szuletesiDatum;
+            this.nemzetiseg = nemzetiseg;
+        }
+
+        public string Nev { get => nev; }
+        public string SzuletesiDatum { get => szuletesiDatum; }
+        public string Nemzetiseg { get => nemzetiseg; }
+        public int Rajtszam { get => rajtszam; }
     }
 }
--- Pilotak/Program.cs
+++ Pilotak/Program.cs
@@ -8,13 +8,30 @@
 
             StreamReader streamReader = new StreamReader("pilotak.csv");
             streamReader.ReadLine();
-            string sor;
-            while ((sor = streamReader.ReadLine()) != null)
+            while (!streamReader.EndOfStream)
             {
-                pilotakLista.Add();
+                string[] pilotaAdatai = streamReader.ReadLine().Split(";");
+                if (pilotaAdatai.Count() == 4)
+                {
+                    pilotakLista.Add(new Pilotak(pilotaAdatai[0], pilotaAdatai[1], pilotaAdatai[2]));
+                }
+                else if (pilotaAdatai.Count() == 3)
+                {
+                    pilotakLista.Add(new Pilotak(pilotaAdatai[0], pilotaAdatai[1], pilotaAdatai[2], int.Parse(pilotaAdatai[3])));
+                }
             }
             streamReader.Close();
-            Pilotak pilotak = new Pilotak(streamReader);
+
+            Console.WriteLine($"3. feladat: {pilotakLista.Count}");
+            Console.WriteLine($"4. feladat: {pilotakLista.Last().Nev}");
+            Console.WriteLine("5. feladat: ");
+            foreach (Pilotak pilota in pilotakLista)
+            {
+                if (int.Parse(pilota.SzuletesiDatum.Split(".")[0]) < 1901)
+                {
+                    Console.WriteLine($"\t{pilota.Nev} ({pilota.SzuletesiDatum})");
+                }
+            }
         }
     }
 }
